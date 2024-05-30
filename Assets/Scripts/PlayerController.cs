0e10b98b31929dc2dce207f4b14e5deb93edda73
Artem <gpsqaz1@gmail.com>
2024-05-30 16:51:21 +0700

    Create a code for move and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed = 6.0f;
+    [SerializeField] private float _jumpHeight = 1.5f;
+    [SerializeField] private float _gravity = -9.81f; // -9.81f the closest value of the acceleration of free fall on the Earth
+
+    private CharacterController _characterController;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+
+    private void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _isGrounded = _characterController.isGrounded;
+
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        Move();
+        Jump();
+
+        _velocity.y += _gravity * Time.deltaTime;
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        _characterController.Move(move * _speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
     }
 }
+
+
