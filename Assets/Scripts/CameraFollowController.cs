8b3d2743cf613b203c9c55817ab0ba9085d4b84e
Artem <gpsqaz1@gmail.com>
2024-05-30 22:20:08 +0700

    Camera TPP is done

--- Assets/Scripts/CameraFollowController.cs
+++ Assets/Scripts/CameraFollowController.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class CameraFollowController : MonoBehaviour
 {
-    [SerializeField] private Vector3 _offset;
+ 
     [SerializeField] private Transform _playerTransform;
-    private float _distance = 5.0f;
-    
+    private Vector3 _offset;
+
     private float _sensitivity = 2.5f; 
-    private float _minAngleY = -40f;
-    private float _maxAngleY = 80f;
+    private float _minAngleY = -75f;
+    private float _maxAngleY = 75f;                    
 
     private float _currentX = 0.0f;
     private float _currentY = 0.0f;
 
+    //private float _distance = 5.0f;
+
     private void Start()
     {
-        _offset = new Vector3(0, 2 - _distance);
+        _offset = new Vector3(4.5f, 2.5f, 0.5f);
     }
 
     private void Update()
     {
         _currentX += Input.GetAxis("Mouse X") * _sensitivity;
         _currentY += Input.GetAxis("Mouse Y") * _sensitivity;
-        _currentY = Mathf.Clamp(_currentY, _minAngleY, _maxAngleY);  //limit angels up and down
+        _currentY = Mathf.Clamp(_currentY, _minAngleY, _maxAngleY);  //limit angels up and down     
     }
 
     private void LateUpdate()
     {
         Quaternion rotation = Quaternion.Euler(_currentY, _currentX, 0);
-        transform.position = _playerTransform.position + rotation * _offset;
-        transform.LookAt(_playerTransform.position);
+
+        Vector3 position = _playerTransform.position + rotation * new Vector3(_offset.x, 0, _offset.z);
+        position.y += _offset.y;
+
+        transform.position = position;
+
+        transform.LookAt(_playerTransform.position + Vector3.up * 1.5f);
     }
 }
