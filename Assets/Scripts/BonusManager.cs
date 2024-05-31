192bdd2b1d272ac7bae12776f29d168289e6e979
Artem <gpsqaz1@gmail.com>
2024-05-31 21:50:22 +0700

    finished the logic of continuing the bonus
    when selecting a double jump or acceleration bonus, provided that they are already working, the counter is simply reset

--- Assets/Scripts/BonusManager.cs
+++ Assets/Scripts/BonusManager.cs
@@ -1,10 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BonusManager : MonoBehaviour
 {
-    public static BonusManager Instance { get; private set; }
+    public static BonusManager Instance { get; private set; }       //store a single instance
 
     private void Awake()
     {
@@ -20,31 +19,21 @@ public class BonusManager : MonoBehaviour
 
     public void ActivateBonus(GameObject bonus, PlayerController player)
     {
-        StartCoroutine(EffectDuration(bonus, player));
-    }
-
-    private IEnumerator EffectDuration(GameObject bonus, PlayerController player)
-    {
         Bonus bonusScript = bonus.GetComponent<Bonus>();
         if (bonusScript != null)
-        {
-            bonusScript.ApplyEffect(player);
+        {         
+            bonusScript.ApplyEffect(player); //Applies an effect to the player
             bonus.SetActive(false);
-            Debug.Log($"{bonus.name} deactivated");
+          //  Debug.Log($"{bonus.name} deactivated");
 
-            // Запускаем параллельную корутину для восстановления бонуса через 5 секунд
             StartCoroutine(ReactivateBonus(bonus));
-
-            yield return new WaitForSeconds(10f); // Длительность эффекта 10 секунд
-            bonusScript.RemoveEffect(player);
-            Debug.Log($"{bonus.name} effect removed");
         }
     }
 
     private IEnumerator ReactivateBonus(GameObject bonus)
     {
-        yield return new WaitForSeconds(5f); // Время до повторного появления 5 секунд
-        Debug.Log($"{bonus.name} reactivated");
-        bonus.SetActive(true); // Активируем объект после прошествия 5 секунд
+        yield return new WaitForSeconds(5f);
+      //  Debug.Log($"{bonus.name} reactivated");
+        bonus.SetActive(true);
     }
 }
