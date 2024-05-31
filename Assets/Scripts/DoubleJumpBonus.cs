5c5ad75edfdd77e77d8dc88f6c9ece159420b24a
Artem <gpsqaz1@gmail.com>
2024-05-31 21:48:53 +0700

    simplified the code

--- Assets/Scripts/DoubleJumpBonus.cs
+++ Assets/Scripts/DoubleJumpBonus.cs
@@ -4,47 +4,15 @@ using UnityEngine;
 
 public class DoubleJumpBonus : Bonus
 {
-    private bool _active = false;
-
     public override void ApplyEffect(PlayerController player)
     {
-        player.EnableDoubleJump();
-        _active = true;
+        player.EnableDoubleJump();      
     }
 
     public override void RemoveEffect(PlayerController player)
-    {
-        if (_active)
-        {
-            player.DisableDoubleJump();
-            _active = false;
-        }
+    {    
+         player.DisableDoubleJump();       
     }
 
-    /* private PlayerController _playerController;
-     private float _effectDuration = 10f;
-     private bool _active = false;
-
-     protected override void ApplyEffect(PlayerController player)
-     {
-         _playerController = player;
-         _playerController.EnableDoubleJump();
-         _active = true;
-         StartCoroutine(EffectDuration());
-     }
-
-     protected override void RemoveEffect(PlayerController player)
-     {
-         if (_active)
-         {
-             _playerController.DisableDoubleJump();
-             _active = false;
-         }
-     }
-
-     private IEnumerator EffectDuration()
-     {
-         yield return new WaitForSeconds(_effectDuration);
-         RemoveEffect(_playerController);
-     }*/
+    
 }
--- Assets/Scripts/SpeedBoostBonus.cs
+++ Assets/Scripts/SpeedBoostBonus.cs
@@ -4,47 +4,14 @@ using UnityEngine;
 
 public class SpeedBoostBonus : Bonus
 {
-
-    private bool _active = false;
-
     public override void ApplyEffect(PlayerController player)
     {
         player.EnableSpeedBoost();
-        _active = true;
     }
 
     public override void RemoveEffect(PlayerController player)
-    {
-        if (_active)
-        {
-            player.DisableSpeedBoost();
-            _active = false;
-        }
+    {      
+         player.DisableSpeedBoost();          
     }
-    /*private PlayerController _playerController;
-    private float _effectDuration = 10f;
-    private bool _active = false;
-
-    protected override void ApplyEffect(PlayerController player)
-    {
-        _playerController = player;
-        _playerController.EnableSpeedBoost();
-        _active = true;
-        StartCoroutine(EffectDuration());
-    }
-
-    protected override void RemoveEffect(PlayerController player)
-    {
-        if (_active)
-        {
-            _playerController.DisableSpeedBoost();
-            _active = false;
-        }
-    }
-
-    private IEnumerator EffectDuration()
-    {
-        yield return new WaitForSeconds(_effectDuration);
-        RemoveEffect(_playerController);
-    }*/
+    
 }
