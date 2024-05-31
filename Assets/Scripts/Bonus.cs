9222d88562443e3e9600ac908f5d1750b20dbf8e
Artem <gpsqaz1@gmail.com>
2024-05-31 16:53:42 +0700

    Create main abstract class for bonuses

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Bonus : MonoBehaviour
+{
+    public abstract void ApplyEffect(PlayerController player);
+    public abstract void RemoveEffect(PlayerController player);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                BonusManager.Instance.ActivateBonus(gameObject, player);
+            }
+        }
+    }
+}
